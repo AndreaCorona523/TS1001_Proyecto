489182da855d2f36e567764c6f0d1373e3791062
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2021-10-29 10:55:18 -0500

    Update librerias_kernel.py

--- librerias_kernel.py
+++ librerias_kernel.py
@@ -3,21 +3,21 @@ import cv2
  
 #Funcion para el Kernel del sombrero 
 #Parametros iniciales valor de sigma y tamaño de matriz (k)
-def kernel_sombrero(sigma,k):
-    matrix=numpy.zeros((k,k)); #Crear matriz de K x K con valor 0
+def kernel_sombrero(sigma,k): 
+    matrix=numpy.zeros((k,k));  #Crear matriz de K x K con valor 0
     for x in range (0,k): # For para recorer todos los valores en x 
         for y in range (0,k): #For para recorer todos los valores en y
-      #Formula para el filtro del sombrero en 2D
-            matrix[x][y]=1/(numpy.pi*sigma**4)*(1-1/2*(x**2+y**2)/sigma**2)
-            matrix[x][y]= matrix[x][y] * numpy.exp(-(x**2+y**2)/(2*sigma**2))
+            #Formula para el filtro del sombrero en 2D
+            matrix[x][y]=1/(numpy.pi*sigma**4)*(1-1/2*(x**2+y**2)/sigma**2) \
+                         * numpy.exp(-(x**2+y**2)/(2*sigma**2))
     return matrix #Regresamos la matriz con el filtro 
-
+   
 def gauss(sigma,k):
     g=numpy.zeros((k,k));
     for x in range (0,k):
         for y in range (0,k):
             g[x][y]=1/(2*numpy.pi*sigma**2)*numpy.exp(-(x**2+y**2)/(2*sigma**2))
-    return g
+    return g #Regresamos la matriz con el filtro 
    
    
 #Aquí se tiene el segmento utilizado para el filtro laplaciano que ocupa antes
@@ -25,14 +25,14 @@ def gauss(sigma,k):
 
 def LoG(sigma, x, y):
     laplace = -1/(numpy.pi*sigma**4)(1-(x**2+y**2)/(2*sigma**2))*numpy.exp(-(x**2+y**2)/(2*sigma**2))
-    return laplace
+    return laplace #Regresamos la matriz con el filtro 
 
 def LoG_discrete(sigma, k):
     l = numpy.zeros((k,k))
     for i in range(k):
         for j in range(k):
             l[i,j] = LoG(sigma, (i-(k-1)/2),(j-(k-1)/2))
-    return l
+    return l #Regresamos la matriz con el filtro 
 
 def sepia():
   img = cv2.imread('Sample.png')
@@ -43,4 +43,4 @@ def sepia():
   img[numpy.where(img > 255)] = 255 # normalizando valores mayores que 255 
   cv2.waitKey(0)
   cv2.destroyAllWindows
-  return img
+  return img #Regresamos la matriz con el filtro 
