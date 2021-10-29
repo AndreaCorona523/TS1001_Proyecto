72dc9f84f1d51da4b109f6490a1778aafd779853
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2021-10-29 11:49:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def procesamiento_imagen(imagen):
     plt.show()
 
 if __name__ == "__main__":
-    #Obtener imagen de carpeta de imagenes
+    #get image arguments from the shell  "python main.py imagenes/Sample.png"
     ap = argparse.ArgumentParser()
     ap.add_argument("image", help="Path to the image", type=str)
     args = ap.parse_args()
